d4d2a66740c16d2be28e9377bb03d2b4262c2d16
seshghie <seshghie@sfu.ca>
2020-06-27 15:52:05 -0700

    Help screen added.
    Added Help screen activity with all applicable classes and layouts. Currently bare, awaiting development upon consultation with team.

---
+++ app/src/main/java/cmpt276/termproject/ui/HelpScreen.java
@@ -0,0 +1,19 @@
+package cmpt276.termproject.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import cmpt276.termproject.R;
+
+public class HelpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_screen);
+    }
+}
