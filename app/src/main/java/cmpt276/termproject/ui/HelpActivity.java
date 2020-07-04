21f22c32166ab54252757d93cac680d1523aa7ab
nargisgh <56950238+nargisgh@users.noreply.github.com>
2020-07-04 13:48:26 -0700

    Setting Sources textview as scrolling text

--- app/src/main/java/cmpt276/termproject/ui/HelpActivity.java
+++ app/src/main/java/cmpt276/termproject/ui/HelpActivity.java
@@ -6,8 +6,10 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import cmpt276.termproject.R;
 import cmpt276.termproject.model.MusicManager;
@@ -16,6 +18,8 @@ public class HelpActivity extends AppCompatActivity {
 
     ConstraintLayout hs_Layout;
     public MusicManager musicManager;
+    TextView sources;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +28,13 @@ public class HelpActivity extends AppCompatActivity {
 
         musicManager = MusicManager.getInstance();
         musicManager.play();
+
         setupBackButton();
 
         hs_Layout = findViewById(R.id.hs_Layout);
         hs_Layout.setBackgroundResource(R.drawable.bg_help);
+        sources = findViewById(R.id.sources);
+        sources.setMovementMethod(new ScrollingMovementMethod());
     }
 
 
