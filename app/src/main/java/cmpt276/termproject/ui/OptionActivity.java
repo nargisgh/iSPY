50158cb6a30a8d3a87d61fe25b360248347f7910
seshghie <seshghie@sfu.ca>
2020-07-03 19:49:21 -0700

    Options screen wired up and complete.
    The radio buttons on the options screen that are used to select the theme are now fully functional and save/retrieve to/from shared preferences between application runs. The options screen is now complete.

--- app/src/main/java/cmpt276/termproject/ui/OptionActivity.java
+++ app/src/main/java/cmpt276/termproject/ui/OptionActivity.java
@@ -5,9 +5,14 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import cmpt276.termproject.R;
 import cmpt276.termproject.model.MusicManager;
@@ -15,7 +20,14 @@ import cmpt276.termproject.model.MusicManager;
 public class OptionActivity extends AppCompatActivity {
 
     ConstraintLayout os_Layout;
+    RadioGroup theme_grp;
+    SharedPreferences mPreferences;
+    SharedPreferences.Editor mEdit;
+    RadioButton hero_rbtn;
+    RadioButton hypno_rbtn;
     public MusicManager musicManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +38,11 @@ public class OptionActivity extends AppCompatActivity {
         musicManager.play();
         os_Layout = findViewById(R.id.os_Layout);
         os_Layout.setBackgroundResource(R.drawable.bg_options);
+        hero_rbtn = findViewById(R.id.options_heroes_rbtn);
+        hypno_rbtn = findViewById(R.id.options_hypno_rbtn);
 
         setupBackButton();
+        storeOptions();
     }
 
 
@@ -37,6 +52,52 @@ public class OptionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+    }
+
+    private void storeOptions()
+    {
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        mEdit = mPreferences.edit();
+        String curr_theme = mPreferences.getString("Theme", "Superheroes");
+
+        if (curr_theme.equals("Hypnomob"))
+        {
+            hypno_rbtn.setChecked(true);
+        }
+        else
+        {
+            hero_rbtn.setChecked(true);
+        }
+
+        theme_grp = findViewById(R.id.options_themes_rgroup);
+        theme_grp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId)
+            {
+
+                View theme_btn = theme_grp.findViewById(checkedId);
+                int index = theme_grp.indexOfChild(theme_btn);
+
+                switch (index)
+                {
+                    case 1: //Superhero theme chosen
+
+                        mEdit.putString("Theme", "Superheroes");
+                        mEdit.commit();
+                        Toast.makeText(getApplicationContext(), "Superhero theme applied!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case 2: //Hypnomob theme chosen
+
+                        mEdit.putString("Theme", "Hypnomob");
+                        mEdit.commit();
+                        Toast.makeText(getApplicationContext(), "Hypnomob theme applied!", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
             }
         });
     }
