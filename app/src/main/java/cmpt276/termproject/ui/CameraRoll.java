9df1609678923d53cfbf2e1c1777c39adeea22ef
nargisgh <56950238+nargisgh@users.noreply.github.com>
2020-07-23 00:08:23 -0700

    Adding layout for Camera roll calling adapter and inflating items_gallery xml and button to go back.

--- app/src/main/java/cmpt276/termproject/ui/CameraRoll.java
+++ app/src/main/java/cmpt276/termproject/ui/CameraRoll.java
@@ -1,16 +1,20 @@
 package cmpt276.termproject.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TableLayout;
-import android.widget.TableRow;
 
 import java.util.List;
 
@@ -22,35 +26,76 @@ public class CameraRoll extends AppCompatActivity {
 
     private FlickrManager flickrManager;
     private List<FlickrImage> imageList;
-    private TableLayout tableLayout;
+    private RecyclerView mPhotoRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_roll);
 
-        flickrManager = FlickrManager.getInstance();
-        tableLayout = findViewById(R.id.camera_roll_table);
+        setUpBack();
 
+        flickrManager = FlickrManager.getInstance();
         showBitmaps();
+        mPhotoRecyclerView = findViewById(R.id.camroll);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(CameraRoll.this,5));
+        mPhotoRecyclerView.setAdapter(new PhotoHolder());
+
     }
 
-    public void showBitmaps(){
-        imageList = flickrManager.getImageList(getApplicationContext());
+    private void setUpBack() {
+        Button back = findViewById(R.id.cam_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 
-        for (int i = 0; i < imageList.size(); i ++){
+    public class PhotoHolder extends RecyclerView.Adapter<ImageViewHolder>{
 
-            TableRow tr = new TableRow(this);
-            ImageView imageView = new ImageView(this);
-            imageView.setImageBitmap(imageList.get(i).getImgBitmap());
-            tr.addView(imageView);
-            tableLayout.addView(tr);
+        @Override
+        public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(CameraRoll.this);
+            View view = inflater.inflate(R.layout.list_item_gallery, parent, false);
+            ImageViewHolder imageViewHolder = new ImageViewHolder(view);
 
+            return imageViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ImageViewHolder holder, int position) {
+            holder.image.setImageBitmap(imageList.get(position).getImgBitmap());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return imageList.size();
         }
     }
 
+    public static class ImageViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        CheckBox checkBox;
+        public ImageViewHolder( View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.item_image_view);
+            checkBox = itemView.findViewById(R.id.grid_item_checkbox);
+
+        }
+    }
+    public void showBitmaps(){
+        imageList = flickrManager.getImageList(getApplicationContext());
+    }
 
     public static Intent makeIntent(Context context){
         return new Intent(context,CameraRoll.class);
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 }
